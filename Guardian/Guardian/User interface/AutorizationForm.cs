b9ef91a5d02461af3ef7cb5e8020af0539966f28
Меркульев Н.К <nickitos042004@gmail.com>
2023-12-13 13:06:26 +0400

    добавил package user interface и форму авторизации

--- Guardian/Guardian/User interface/AutorizationForm.cs
+++ Guardian/Guardian/User interface/AutorizationForm.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Guardian
+namespace Guardian.User_interface
 {
-    public partial class Form1 : Form
+    public partial class AutorizationForm : Form
     {
-        public Form1()
+        public AutorizationForm()
         {
             InitializeComponent();
         }
